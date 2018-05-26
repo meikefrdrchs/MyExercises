3fd3a02ac59feab80e91ad131f9e8b59324e334e
meikefrdrchs <friedrichs.meike@web.de>
2018-05-27 00:39:24 +0200

    added first python script

---
+++ 2018_05_22.py
@@ -0,0 +1,36 @@
+firstname = "Meike"
+lastname = "Friedrichs"
+
+def intro(firstname,lastname):
+    print("Hello stranger, my name is",firstname,lastname+".")
+
+intro(firstname,lastname)
+
+def desc1():
+    print("I'm working on my first python project for my Tech Basics class at university and I'm sort of confused about everything.")
+
+def desc2():
+    print("So please don't judge my poor python skills!")
+
+def desc():
+    desc1()
+    desc2()
+
+desc()
+
+firstname = firstname.replace("e","3")
+firstname = firstname.replace("i","1")
+firstname = firstname.replace("m","w")
+
+lastname = lastname.replace("e","3")
+lastname = lastname.replace("i","1")
+lastname = lastname.replace("d","0")
+lastname = lastname.replace("s","9")
+
+newname = firstname,lastname
+
+alias = ''.join(newname)
+alias = alias.lower()
+
+
+print("If I had to choose an alias, it would probably be",alias+"!")
