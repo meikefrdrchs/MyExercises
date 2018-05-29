1786839d67fd47ce939e1b067d14c2ab8fa4ad84
meikefrdrchs <friedrichs.meike@web.de>
2018-05-29 21:46:00 +0200

    added assignment

---
+++ assignment_2018_05_29.py
@@ -0,0 +1,6 @@
+random_animals = "cats dogs horses mice turtles lions bears cats meerkats dogs alpacas dogs cats horses dogs turtles cats dogs spiders bees dogs".split("cats")
+
+print("These are some of the best animals:")
+
+for animals_that_arent_cats in random_animals:
+    print(animals_that_arent_cats)
